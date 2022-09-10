81f0a7e6aae616f7bca3d9d64d3c412ee8f9910f
ambono <modpleh86@gmail.com>
2022-09-10 14:54:43 +0100

    attempt to fix app file not found issue

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import './i18n';
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
-//import Main from "./Main";
 import './services/i18n';
-import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -33,10 +12,4 @@ ReactDOM.render(
     document.getElementById("root")
   );
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
 serviceWorker.register();
